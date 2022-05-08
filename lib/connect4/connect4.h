27158a60932670e40ed3394ebf675de24d99fe12
Romelianism <Romelianism@gmail.com>
2022-05-08 21:42:15 +0800

    feat: add connect4 struct

--- lib/connect4/connect4.h
+++ lib/connect4/connect4.h
@@ -1,6 +1,24 @@
 #ifndef CONNECT4_H
 #define CONNECT4_H
 
-void connect4();
+#include <stddef.h>
+#include <stdbool.h>
+
+#define CONNECT4_WIDTH 7
+#define CONNECT4_HEIGHT 6
+
+struct Connect4
+{
+    // The Connect 4 board grid
+    enum Tile
+    {
+        empty,
+        player1,
+        player2
+    } board[CONNECT4_WIDTH][CONNECT4_HEIGHT];
+
+    // Which player is in turn
+    bool turn;
+};
 
 #endif
