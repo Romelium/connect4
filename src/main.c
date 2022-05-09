45a2f87a40241d7bbb3039809863535dc71f07d0
Romelianism <Romelianism@gmail.com>
2022-05-09 16:38:38 +0800

    feat: add playable game

--- src/main.c
+++ src/main.c
@@ -4,13 +4,26 @@
 int main()
 {
     struct Connect4 connect4 = {.board = {0}, .turn = false};
-
-    connect4_drop(&connect4, 0, true);
-    connect4_drop(&connect4, 5, true);
-    connect4_drop(&connect4, 2, true);
-    connect4_drop(&connect4, 2, true);
-    connect4_drop(&connect4, 2, true);
-    connect4_drop(&connect4, 0, true);
     connect4_print(&connect4);
+
+    while (1)
+    {
+        int input;
+        scanf("%d", &input);
+
+        connect4_drop(&connect4, input, true);
+        connect4_print(&connect4);
+
+        switch (connect4_check_win(&connect4))
+        {
+        case winner_player1:
+            printf("winner: player 1!\n");
+            goto end;
+        case winner_player2:
+            printf("winner: player 2!\n");
+            goto end;
+        }
+    }
+end:
     return 0;
 }
